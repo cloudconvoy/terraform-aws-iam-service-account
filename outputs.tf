fd4253954065c701ecaba467ae5a1100711b4b81
jasonwalsh <jasonwalsh@tutanota.com>
2022-02-01 13:49:29 -0500

    feat: add `service_account_name` output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "role" {
   description = "The role which is linked to the service account"
   value       = aws_iam_role.this
 }
+
+output "service_account_name" {
+  description = "The name of the service account"
+  value       = kubernetes_service_account.this.metadata[0].name
+}
