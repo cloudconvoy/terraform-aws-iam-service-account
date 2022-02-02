1a44e8204dcc9b968c0f5677f8ccf717fab0b66e
jasonwalsh <jasonwalsh@tutanota.com>
2022-02-02 10:44:21 -0500

    feat: output to show namespace of service account

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "service_account_name" {
   description = "The name of the service account"
   value       = kubernetes_service_account.this.metadata[0].name
 }
+
+output "service_account_namespace" {
+  description = "The namespace where the service account is located in"
+  value       = kubernetes_service_account.this.metadata[0].namespace
+}
